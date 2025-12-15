9361812266936f2320671b7ac524d4c830434c32
ramo1990 <abudul917@gmail.com>
2025-12-15 10:12:45 +0100

    Fix: remove frontend submodule and track it as normal directory

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-blue-500 text-white">
+      <h1 className="text-4xl font-bold">Tailwind CSS fonctionne </h1>
+    </main>
+  );
+}
