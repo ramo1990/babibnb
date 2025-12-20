c19556e75baf2f30e7884697d870fbb3f3bed5f0
ramo1990 <abudul917@gmail.com>
2025-12-20 16:37:00 +0100

    connexion avec facebook

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,15 +3,25 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images : {
     remotePatterns: [
+      // Django media
       {
         protocol: "http",
         hostname: "127.0.0.1",
         port: "8001", // important si ton backend tourne sur ce port
         pathname: "/media/**", // autorise toutes les images
       },
+
+      // Google OAuth avatars
       {
         protocol: 'https',
         hostname: '*.googleusercontent.com', // si tu utilises Google Auth ou images Google
+        pathname: "/**",
+      },
+
+      // Facebook OAuth avatars
+      {
+        protocol: "https",
+        hostname: "platform-lookaside.fbsbx.com",
         pathname: "/**",
       },
     ]
